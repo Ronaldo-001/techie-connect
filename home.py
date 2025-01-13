cff04db086f84265f5875a083403f3ab4e7bb00d
Ronaldo-001 <ronaldop1705@gmail.com>
2025-01-13 17:50:00 +0000

    added streamlit template

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# page title
+st.title("Techie-Connect")
+
+st.divider() # placing dividor
+
+st.write("Welcome to my app!!")
+st.write("Just fill the form to get started!!")
+
+st.divider()
+
+st.subheader("Fill the form now to get Matched") #add a subheading
